757b87e6dd8e2c6fcf2c64615df7b23af7eb0f7d
Dimitar Gueorguiev <dimitar_pg13@hotmail.com>
2015-10-04 11:36:35 -0400

    committing Boost examples 10-4

--- ConcurrencyInAction/src/ConcurrencyInAction.cpp
+++ ConcurrencyInAction/src/ConcurrencyInAction.cpp
@@ -7,16 +7,50 @@
 //============================================================================
 
 #include <iostream>
-using namespace std;
+#include <thread>
+#include <pthread.h> // for the pthread example
+
+
 
 void do_some_work()
 {
+   std::cout << "I am doing work at the moment..." << std::endl;
+}
+
+void * call_from_posix_thread(void *)
+{
+	std::cout << "Launched by a posix thread..." << std::endl;
+
+	return NULL;
+}
+
+void call_a_thread_example()
+{
+	std::cout << "call_a_thread_example:" << std::endl;
+	std::thread t1(do_some_work);
+	t1.join();
+	std::cout << "Joined the thread. exiting..." << std::endl;
+}
+
+void call_a_posix_thread_example()
+{
+	std::cout << "call_a_posix_thread_example:" << std::endl;
+    pthread_t t;
+
+    pthread_create(&t, NULL, call_from_posix_thread, NULL);
+
+    // join the thread with the main thread
+    pthread_join(t, NULL);
+    std::cout << "Joined the posix thread. exiting..." << std::endl;
 
 }
 
-
-
 int main() {
-	cout << "Concurrency in Action examples" << endl; // prints Concurrency in Action examples
+	std::cout << "Concurrency in Action examples" << std::endl; // prints Concurrency in Action examples
+
+	call_a_thread_example();
+    call_a_posix_thread_example();
+
+
 	return 0;
 }
