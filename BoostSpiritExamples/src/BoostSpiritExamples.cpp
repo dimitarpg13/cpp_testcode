a443558f97a07579789f96c54252fe52fbc938e4
Dimitar Gueorguiev <dimitar_pg13@hotmail.com>
2015-10-16 19:34:15 -0400

    Boost examples 10-16-15

--- BoostSpiritExamples/src/BoostSpiritExamples.cpp
+++ BoostSpiritExamples/src/BoostSpiritExamples.cpp
@@ -176,7 +176,105 @@ namespace client
        return r;
    }
 
-}
+
+   // parse roman hundreds (100..900) numerals using a symbol table
+   // notice that the data associated with each slot is the parser's
+   // attribute (which is passed to attached semantic actions).
+   //[tutorial_roman_hundreds
+   struct hundreds_ : qi::symbols<char, unsigned>
+   {
+   	hundreds_()
+   	{
+   	   add
+   	   	   ("C", 100)
+   	   	   ("CC", 200)
+   	   	   ("CCC", 300)
+   	   	   ("CD", 400)
+   	   	   ("D", 500)
+   	   	   ("DC", 600)
+   	   	   ("DCC", 700)
+   	   	   ("DCCC", 800)
+   	   	   ("CM", 900)
+   	   ;
+   	}
+
+   } hundreds;
+   //]
+
+   //parse roman tens (10..90) using the symbol table
+   //[tutorial_roman_tens
+   struct tens_ : qi::symbols<char, unsigned>
+   {
+	  tens_()
+	  {
+		 add
+		     ("X", 10)
+		     ("XX", 20)
+		     ("XXX", 30)
+		     ("XL", 40)
+		     ("L", 50)
+		     ("LX", 60)
+		     ("LXX", 70)
+		     ("LXXX", 80)
+		     ("XC", 90)
+		 ;
+	  }
+
+   } tens;
+   //]
+
+   //parse roman ones (1..9) using the symbol table
+   //[tutorial_roman_ones
+   struct ones_ : qi::symbols<char, unsigned>
+   {
+   	  ones_()
+   	  {
+   		 add
+   		     ("I", 1)
+   		     ("II", 2)
+   		     ("III", 3)
+   		     ("IV", 4)
+   		     ("V", 5)
+   		     ("VI", 6)
+   		     ("VII", 7)
+   		     ("VIII", 8)
+   		     ("IX", 9)
+   		 ;
+   	  }
+
+   } ones;
+   //]
+
+
+   //[roman numerals grammar
+   template<typename Iterator>
+   struct roman : qi::grammar<Iterator, unsigned()>
+   {
+	   roman() : roman::base_type(start)
+	   {
+		   using qi::eps;
+		   using qi::lit;
+		   using qi::_val;
+		   using qi::_1;
+		   using ascii::char_;
+
+		   start = eps  	[_val = 0]  >>
+		   (
+			 +lit('M')		[_val += 1000]
+			  ||  hundreds 	[_val += _1]
+			  ||  tens		[_val += _1]
+			  ||  ones  	[_val += _1]
+		   )
+		   ;
+	   }
+
+	   qi::rule<Iterator, unsigned()> start;
+   };
+
+};
+
+
+
 
 void spirit_comma_separated_parser1()
 {
@@ -322,6 +420,43 @@ void spirit_comma_separated_list_parser()
 	}
 }
 
+
+void spirit_roman_numerals_parser()
+{
+	typedef std::string::const_iterator iterator_type;
+	typedef client::roman<iterator_type> roman;
+
+	roman roman_parser; //our grammar
+
+	std::string str;
+	unsigned result;
+	while (std::getline(std::cin, str))
+	{
+		if (str.empty() || str[0] == 'q' || str[0] == 'Q')
+		  break;
+
+		std::string::const_iterator iter = str.begin();
+        std::string::const_iterator end = str.end();
+        //[tutorial_roman_grammar_parse
+        bool r = parse(iter, end, roman_parser, result);
+
+        if (r && iter == end)
+        {
+        	std::cout << "Parsing succeeded." << std::endl;
+        }
+        else
+        {
+        	std::string rest(iter, end);
+        	std::cout << "Parsing failed. Stopped at: \"" << rest << "\"" << std::endl;
+        }
+        //]
+
+
+	}
+
+
+
+}
 
 
 int main() {
