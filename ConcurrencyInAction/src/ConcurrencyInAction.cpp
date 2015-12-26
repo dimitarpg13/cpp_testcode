a93fa53bcd18a93670035211774a94c689dc2dc6
Dimitar Gueorguiev <dimitar_pg13@hotmail.com>
2015-12-26 13:15:16 -0500

    some mutex examples

--- ConcurrencyInAction/src/ConcurrencyInAction.cpp
+++ ConcurrencyInAction/src/ConcurrencyInAction.cpp
@@ -22,6 +22,8 @@
 #include <exception>
 #include <cstdlib>
 #include <time.h>
+#include <stdexcept>
+#include <climits>
 
 void do_some_work()
 {
@@ -532,6 +534,116 @@ void threadsafe_swap_example()
 
 };
 
+
+
+class hierarchical_mutex
+{
+	std::mutex internal_mutex;
+	unsigned long const hierarchy_value;
+	unsigned long previous_hierarchy_value;
+	static thread_local unsigned long this_thread_hierarchy_value;
+
+	void check_for_hierarchy_violation()
+	{
+		if (this_thread_hierarchy_value <= hierarchy_value)
+		{
+			throw std::logic_error("mutex hierarchy violated");
+		}
+	}
+
+	void update_hierarchy_value()
+	{
+		previous_hierarchy_value = this_thread_hierarchy_value;
+		this_thread_hierarchy_value = hierarchy_value;
+	}
+
+public:
+	explicit hierarchical_mutex(unsigned level):
+		hierarchy_value(level),
+		previous_hierarchy_value(0)
+	{}
+
+	void lock()
+	{
+		check_for_hierarchy_violation();
+		internal_mutex.lock();
+		update_hierarchy_value();
+	}
+
+	void unlock()
+	{
+		this_thread_hierarchy_value = previous_hierarchy_value;
+		internal_mutex.unlock();
+	}
+
+	bool try_lock()
+	{
+		check_for_hierarchy_violation();
+		if (!internal_mutex.try_lock())
+		{
+			return false;
+		}
+
+		update_hierarchy_value();
+		return true;
+	}
+};
+
+thread_local unsigned long
+	hierarchical_mutex::this_thread_hierarchy_value(ULONG_MAX);
+
+hierarchical_mutex high_level_mutex(10000);
+hierarchical_mutex low_level_mutex(5000);
+
+int do_low_level_stuff()
+{
+	std::cout << "Calling do_low_level_stuff()" << std::endl;
+	return 42;
+};
+
+int low_level_func()
+{
+	std::lock_guard<hierarchical_mutex> lk(low_level_mutex);
+	return do_low_level_stuff();
+};
+
+void high_level_stuff(int some_param)
+{
+	std::cout << "Calling from high_level_stuff(int some_param) with some_param = " << some_param << std::endl;
+};
+
+void high_level_func()
+{
+	std::lock_guard<hierarchical_mutex> lk(high_level_mutex);
+	high_level_stuff(low_level_func());
+};
+
+void thread_a()
+{
+	high_level_func();
+};
+
+hierarchical_mutex other_mutex(100);
+void do_other_stuff()
+{
+  std::cout << "Calling from do_other_stuff()" << std::endl;
+};
+
+void other_stuff()
+{
+	high_level_func();
+	do_other_stuff();
+}
+
+void thread_b()
+{
+	std::lock_guard<hierarchical_mutex> lk(other_mutex);
+	other_stuff();
+}
+
+
+
+
 void threadsafe_stack_example()
 {
 	threadsafe_stack<int> si;
@@ -666,7 +778,7 @@ int main() {
 	//protecting_shared_data_example();
     //spawn_threads_example();
     //threadsafe_stack_example();
-    threadsafe_swap_example();
+    //threadsafe_swap_example();
 
 	return 0;
 }
