76b4e33531a62230dba956324de6d4bb1ef04d7b
Dimitar Gueorguiev <dimitar_pg13@hotmail.com>
2016-01-17 12:29:39 -0500

    Mem leak finder 1/17/16..work in progress

---
+++ MemoryLeakFinder/src/MemoryLeakFinder.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : MemoryLeakFinder.cpp
+// Author      : Dimitar Gueorguiev
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Memory Leak Finder" << endl; // prints Memory Leak Finder
+	return 0;
+}
