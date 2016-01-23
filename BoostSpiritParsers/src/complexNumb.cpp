efe8a64ee5cf098845748f5f02334809080e69ad
Dimitar Gueorguiev <dimitar_pg13@hotmail.com>
2016-01-23 18:46:06 -0500

    Boost qi examples

---
+++ BoostSpiritParsers/src/complexNumb.cpp
@@ -0,0 +1,16 @@
+/*
+ * complexNumb.cpp
+ *
+ *  Created on: Jan 23, 2016
+ *      Author: root
+ */
+#include "complexNumb.h"
+namespace client
+{
+    bool parse_print_complexnumb(std::string file)
+    {
+
+	  return true;
+    }
+}
+
---
+++ BoostSpiritParsers/src/complexNumb.h
@@ -0,0 +1,45 @@
+#include <boost/config/warning_disable.hpp>
+#include <boost/spirit/include/qi.hpp>
+#include <boost/spirit/include/phoenix_core.hpp>
+#include <boost/spirit/include/phoenix_operator.hpp>
+
+#include <iostream>
+#include <string>
+#include <complex>
+
+
+// our complex number parser/compiler
+namespace client
+{
+   template<typename Iterator>
+   bool parse_complex(Iterator first, Iterator last, std::complex<double>& c)
+   {
+	   using boost::spirit::qi::double_;
+	   using boost::spirit::qi::_1;
+	   using boost::spirit::qi::phrase_parse;
+	   using boost::spirit::ascii::space;
+	   using boost::phoenix::ref;
+
+	   double rN = 0.0;
+	   double iN = 0.0;
+	   bool r = phrase_parse(first, last,
+
+	      // begin grammar
+		  (
+				  '(' >> double_[ref(rN) = _1]
+				      >> -(',' >> double_[ref(iN) = _1]) >> ')'
+			    |	double_[ref(rN) = _1]
+		  ),
+		  // end grammar
+
+		  space);
+
+	   if (!r || first != last)  // fail if we did not get a full match
+		   return false;
+	   c = std::complex<double>(rN,iN);
+	   return r;
+   }
+
+   bool parse_print_complexnumb(std::string file);
+
+}
